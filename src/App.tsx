ed8e600c29e458a40ce1a42726b79ac41b30c775
Markuff1 <marknixon1@outlook.com>
2025-06-17 15:30:22 +0100

    Added google analysis

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import ReactGA from "react-ga4";
 import Home from "./Home";
 
+// Replace this with your actual Measurement ID
+const GA_MEASUREMENT_ID = "G-8TM3CJK34G";
+
+// Hook to track page views on route change
+function usePageViews() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
+}
+
+// Main App Content
+function AppContent() {
+  usePageViews();
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+// App with GA initialization
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(GA_MEASUREMENT_ID);
+  }, []);
+
   return (
     <Router basename="/mark-cv">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <AppContent />
     </Router>
   );
 }
